cdcf68ab6fa2385278a4686f42875c9d1e98ba2f
Stephany <136421961+splacito22@users.noreply.github.com>
2023-07-19 23:18:59 -0700

    created if statement for password length

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ function generatePassword() {
   var passwordLength = parseInt(
     prompt("Enter the password length (8 to 128 characters):")
   );
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeSpecialChars = confirm("Include special characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+
+  if (!passwordLength || passwordLength < 8 || passwordLength > 128)
+    alert(
+      "Please enter ['Please enter a valid password length between 8 and 128 characters."
+    );
+  return;
 }
 
 // Write password to the #password input
